667f704ea2a802f89bbdb4f5dacdaed5eedbbb4e
Nowhere004 <arcanakgul@hotmail.com>
2019-02-11 23:09:15 +0300

    2D_FollowAI

---
+++ 2D FOLLOW AI/Assets/EnemyFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour {
+
+    public float speed;
+    private Transform target;
+    private float distance;
+
+	void Start () {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        distance = Vector2.Distance(transform.position,target.position);
+        if (distance>=3f)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+        
+	}
+}
---
+++ 2D FOLLOW AI/Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    public float speed;
+    private float moveHor;
+    private float moveVer;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+        moveHor = Input.GetAxisRaw("Horizontal");
+        moveVer = Input.GetAxisRaw("Vertical");
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveHor*speed*Time.fixedDeltaTime,moveVer*speed*Time.fixedDeltaTime);
+    }
+}
