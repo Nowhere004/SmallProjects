9131ba3f85f57021de7e308e1abeba65bc4abbeb
Nowhere004 <arcanakgul@hotmail.com>
2019-02-11 23:13:43 +0300

    Retreat_EnemyAI

---
+++ SHOOTING-FOLLOW-RETREAT ENEMY AI/Assets/Enemy.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed;
+    public float stoppingDistance;
+    public float retreatDistance;
+
+    private Transform player;
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+    public GameObject projectTile;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        timeBtwShots = startTimeBtwShots;
+    }
+
+    private void Update()
+    {
+        if (Vector2.Distance(transform.position,player.position)>stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position,player.position,speed*Time.deltaTime);
+        }
+        else if (Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance)
+        {
+            transform.position = this.transform.position;
+        }
+        else if (Vector2.Distance(transform.position, player.position) <retreatDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
+
+        if (timeBtwShots<=0)
+        {
+            Instantiate(projectTile,transform.position,Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+}
---
+++ SHOOTING-FOLLOW-RETREAT ENEMY AI/Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody2D rb;
+    public float speed;
+    private Vector2 moveVel;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveVel = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+	}
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position+moveVel*speed*Time.fixedDeltaTime);
+    }
+}
