5cb44f54a2caac078a7b6acd48f7177f25a4d775
Nowhere004 <arcanakgul@hotmail.com>
2019-02-11 23:24:42 +0300

    PatrolAI

---
+++ PATROL AI/Assets/Patrol.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour {
+
+    /// <summary>
+    /// Bu kod dizinimizde bizim belirlediğimiz noktalar arasında random bir tane seçip dolaşan bir düşman yapay zekası yaptık
+    /// </summary>
+    public float speed;
+    public Transform[] moveSpots;
+    private int randomSpotIndex;
+    private float waitTime;
+    public float startWaitTime;
+
+    private void Start()
+    {
+        waitTime = startWaitTime;
+        randomSpotIndex = Random.Range(0,moveSpots.Length);
+    }
+
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpotIndex].position, speed * Time.deltaTime);
+        if (Vector2.Distance(transform.position,moveSpots[randomSpotIndex].position)<0.2f)
+        {
+            if (waitTime<=0)
+            {
+                randomSpotIndex = Random.Range(0, moveSpots.Length);
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+            
+        }
+        
+    }
+}
---
+++ PATROL AI/Assets/PatrolRandomCamera.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolRandomCamera : MonoBehaviour {
+
+    /// <summary>
+    /// Bu kod dizininde düşman yapay zekamız kamera da belirlediğimiz alan en fazla en az gidebileciği koordinatlar arasında serbest bir hareket yapmaktadır.
+    /// </summary>
+
+    public float speed;
+    public Transform moveSpot;    
+    private float waitTime;
+    public float startWaitTime;
+
+    public float xMin;
+    public float xMax;
+    public float yMin;
+    public float yMax;
+
+    private void Start()
+    {
+        waitTime = startWaitTime;
+        moveSpot.position = new Vector2(Random.Range(xMin,xMax), Random.Range(yMin, yMax));
+        
+    }
+
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpot.position, speed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, moveSpot.position) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                moveSpot.position = new Vector2(Random.Range(xMin, xMax), Random.Range(yMin, yMax));
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+
+        }
+
+    }
+}
