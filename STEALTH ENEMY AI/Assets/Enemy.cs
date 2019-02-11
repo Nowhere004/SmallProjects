f8f2f13fa78ee51577247a68b1b0d7c3f417ebb2
Nowhere004 <arcanakgul@hotmail.com>
2019-02-11 23:15:14 +0300

    Stealth_EnemyAI

---
+++ STEALTH ENEMY AI/Assets/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    public float rotSpeed;
+    public float distance;
+    public LineRenderer lineofSight;
+    public Gradient redColor;
+    public Gradient greenColor;
+
+
+    private void Start()
+    {
+        Physics2D.queriesStartInColliders = false;//Düşman objemizin kendi Collider'ı ile etkileşime girmesini önledik.
+    }
+    private void Update()
+    {
+        transform.Rotate(Vector3.forward * rotSpeed * Time.deltaTime);
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position,transform.right,distance);
+        if (hitInfo.collider!=null)
+        {
+            lineofSight.SetPosition(1,hitInfo.point);
+            lineofSight.colorGradient = redColor;
+            Debug.DrawLine(transform.position,hitInfo.point,Color.red);
+            if (hitInfo.collider.CompareTag("Player"))
+            {
+                Destroy(hitInfo.collider.gameObject);
+            }
+        }
+        else
+        {
+            Debug.DrawLine(transform.position, transform.position+transform.right*distance, Color.green);
+            lineofSight.SetPosition(1,transform.position+transform.right*distance);
+            lineofSight.colorGradient = greenColor;
+        }
+
+        lineofSight.SetPosition(0,transform.position);
+    }
+
+}
---
+++ STEALTH ENEMY AI/Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    private Rigidbody2D rb;
+    private Vector2 moveVel;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      Vector2  moveInputs = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+        moveVel = moveInputs.normalized * speed; 
+	}
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position+moveVel*Time.fixedDeltaTime);
+    }
+}
