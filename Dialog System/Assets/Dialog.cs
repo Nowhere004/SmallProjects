798a8e586b80deb3eb9d4a2ae93462654bb4f548
Nowhere004 <arcanakgul@hotmail.com>
2019-02-11 23:35:18 +0300

    Dialog_System

---
+++ Dialog System/Assets/Dialog.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Dialog : MonoBehaviour {
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+    public GameObject continueButton;
+    public Animator textDisplayAnim;
+    IEnumerator Type()
+    {
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+
+        }
+
+    }
+
+    private void Start()
+    {
+        StartCoroutine(Type());
+    }
+    private void Update()
+    {
+        if (textDisplay.text==sentences[index])
+        {
+            continueButton.SetActive(true);
+        }
+    }
+
+    public void NextSentence()
+    {
+        textDisplayAnim.SetTrigger("change");
+        continueButton.SetActive(false);
+        if (index<sentences.Length-1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+
+        }
+        else
+        {
+            textDisplay.text = "";
+            continueButton.SetActive(false);
+        }
+    }
+}
