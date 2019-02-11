ff50a43f3d4fab92c40735d0ca675ff3e31dd15c
Nowhere004 <arcanakgul@hotmail.com>
2019-02-11 23:41:31 +0300

    HealthBar

---
+++ HealthBar/Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    private Stat health;
+    [SerializeField]
+    private Stat energy;
+	// Use this for initialization
+	void Awake ()
+    {
+
+        health.Initialize();
+        energy.Initialize();
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            health.CurrentVal -= 10;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            health.CurrentVal += 10;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            energy.CurrentVal -= 10;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            energy.CurrentVal += 10;
+        }
+
+    }
+}
